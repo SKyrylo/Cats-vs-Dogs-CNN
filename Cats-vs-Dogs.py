ba3dc2162b4fda5d182d9b78cb0c5e0723b1ca01
SKyrylo <fursvystun@gmail.com>
2025-01-05 00:00:52 +0200

    added predictions to model

--- Cats-vs-Dogs.py
+++ Cats-vs-Dogs.py
@@ -5,6 +5,7 @@ import os
 import cv2
 # import random
 import pickle
+import keras
 from keras import Sequential, layers
 from keras import callbacks
 import time
@@ -61,30 +62,47 @@ y = np.array(y)
 X = X/255.0
 
 dense_layers = [2]
-layer_sizes = [32, 64, 128]
+layer_sizes = [128]
 conv_layers = [3]
 
-for dense_layer in dense_layers:
-    for layer_size in layer_sizes:
-        for conv_layer in conv_layers:
-            NAME = f'{conv_layer}-conv-{layer_size}-nodes-{dense_layer}-dense-{int(time.time())}'
-            tensorboard = callbacks.TensorBoard(log_dir=f'logs/{NAME}')
+# for dense_layer in dense_layers:
+#     for layer_size in layer_sizes:
+#         for conv_layer in conv_layers:
+#             NAME = f'{conv_layer}-conv-{layer_size}-nodes-{dense_layer}-dense-{int(time.time())}'
+#             tensorboard = callbacks.TensorBoard(log_dir=f'logs/{NAME}')
+#
+#             model = Sequential()
+#             model.add(layers.Conv2D(layer_size, (3, 3), input_shape=X.shape[1:], activation='relu'))
+#             model.add(layers.MaxPooling2D(pool_size=(2, 2)))
+#
+#             for _ in range(conv_layer-1):
+#                 model.add(layers.Conv2D(layer_size, (3, 3), activation='relu'))
+#                 model.add(layers.MaxPooling2D(pool_size=(2, 2)))
+#
+#             model.add(layers.Flatten())
+#
+#             for _ in range(dense_layer):
+#                 model.add(layers.Dense(512, activation='relu'))
+#
+#             model.add(layers.Dense(1, activation='sigmoid'))
+#
+#             model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+#
+#             model.fit(X, y, batch_size=32, validation_split=0.1, epochs=3, callbacks=[tensorboard])
+#
+#             model.save(f'models\\{NAME}.keras', save_format='keras')
 
-            model = Sequential()
-            model.add(layers.Conv2D(layer_size, (3, 3), input_shape=X.shape[1:], activation='relu'))
-            model.add(layers.MaxPooling2D(pool_size=(2, 2)))
 
-            for _ in range(conv_layer-1):
-                model.add(layers.Conv2D(layer_size, (3, 3), activation='relu'))
-                model.add(layers.MaxPooling2D(pool_size=(2, 2)))
+model = keras.models.load_model('models\\3-conv-128-nodes-2-dense-1736011127.keras')
 
-            model.add(layers.Flatten())
 
-            for _ in range(dense_layer):
-                model.add(layers.Dense(512, activation='relu'))
+def prepare(filepath):
+    img_size = 50
+    img_array = cv2.imread(filepath, cv2.IMREAD_GRAYSCALE)
+    new_array = cv2.resize(img_array, (img_size, img_size))
+    return new_array.reshape(-1, img_size, img_size, 1)
 
-            model.add(layers.Dense(1, activation='sigmoid'))
 
-            model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-
-            history = model.fit(X, y, batch_size=32, validation_split=0.1, epochs=3, callbacks=[tensorboard])
+prediction = model.predict([prepare('Cats/cat.1.jpg')])
+prediction2 = model.predict([prepare('Dogs/dog.10.jpg')])
+print(prediction, prediction2)
